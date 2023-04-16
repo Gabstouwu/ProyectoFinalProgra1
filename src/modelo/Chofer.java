2f5cb323998f111202a30f1bb0fa013fd770dbc7
Xhepexz <jjimenez20762@ufide.ac.cr>
2023-04-16 16:41:19 -0600

    Update Chofer.java
    Se crea el modelo de Chofer

--- src/modelo/Chofer.java
+++ src/modelo/Chofer.java
@@ -1,6 +1,38 @@
-
 package modelo;
 
 public class Chofer {
-    //andy inutil
+    private String idPersona;
+    private String vencimientoLicencia;
+
+    public Chofer(String idPersona, String vencimientoLicencia) {
+        this.idPersona = idPersona;
+        this.vencimientoLicencia = vencimientoLicencia;
+    }
+
+    public String getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(String idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getVencimientoLicencia() {
+        return vencimientoLicencia;
+    }
+
+    public void setVencimientoLicencia(String vencimientoLicencia) {
+        this.vencimientoLicencia = vencimientoLicencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Chofer{" + "idPersona=" + idPersona + ", vencimientoLicencia=" + vencimientoLicencia + '}';
+    }
+    
+    
+    
+    
+    
+    
 }
