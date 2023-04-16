ce04fcb1d4d24d56a697b958acb25f3c2f9a4958
Xhepexz <jjimenez20762@ufide.ac.cr>
2023-04-16 16:22:14 -0600

    cambio nombre
    se cambia nombre de package main

--- src/main/MenuPrincipal.java
+++ src/main/MenuPrincipal.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package proyectofinal_v.pkg1;
+package main;
 
 /**
  *
  * @author gabri
  */
-public class ProyectoFinal_V1 {
+public class MenuPrincipal {
 
     /**
      * @param args the command line arguments
