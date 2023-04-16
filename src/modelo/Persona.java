4db8e426e2fd361bd28b83926f6c751646739ddf
Xhepexz <jjimenez20762@ufide.ac.cr>
2023-04-16 16:41:06 -0600

    Update Persona.java
    Se crea el modelo de persona

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -1,6 +1,53 @@
-
 package modelo;
-
 public class Persona {
    
+    private String idPersona;
+    private String nombrePersona;
+    private String telefonoPersona;
+    private String correo;
+
+    public Persona(String idPersona, String nombrePersona, String telefonoPersona, String correo) {
+        this.idPersona = idPersona;
+        this.nombrePersona = nombrePersona;
+        this.telefonoPersona = telefonoPersona;
+        this.correo = correo;
+    }
+
+    public String getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(String idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombrePersona() {
+        return nombrePersona;
+    }
+
+    public void setNombrePersona(String nombrePersona) {
+        this.nombrePersona = nombrePersona;
+    }
+
+    public String getTelefonoPersona() {
+        return telefonoPersona;
+    }
+
+    public void setTelefonoPersona(String telefonoPersona) {
+        this.telefonoPersona = telefonoPersona;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "idPersona=" + idPersona + ", nombrePersona=" + nombrePersona + ", telefonoPersona=" + telefonoPersona + ", correo=" + correo + '}';
+    }
+    
 }
