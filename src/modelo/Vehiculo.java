4061195b40dc29bd765941964941e23fe22ce747
Andyjrr97 <124108114+Andyjrr97@users.noreply.github.com>
2023-04-16 16:45:25 -0600

    Update Vehiculo.java
    se crea modelo de vehiculo

--- src/modelo/Vehiculo.java
+++ src/modelo/Vehiculo.java
@@ -1,13 +1,74 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author gabri
- */
 public class Vehiculo {
-    
+   private String placa; 
+   private String marca;
+   private String estilo;
+   private String modelo;
+   private String capacidadVehiculo;
+
+    public Vehiculo() {
+    }
+
+    public Vehiculo(String placa, String marca, String estilo, String modelo, String capacidadPasajeros) {
+        this.placa = placa;
+        this.marca = marca;
+        this.estilo = estilo;
+        this.modelo = modelo;
+        this.capacidadVehiculo = capacidadPasajeros;
+    }
+
+    public String getCapacidadPasajeros() {
+        return capacidadVehiculo;
+    }
+
+    public void setCapacidadPasajeros(String capacidadPasajeros) {
+        this.capacidadVehiculo = capacidadPasajeros;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getEstilo() {
+        return estilo;
+    }
+
+    public void setEstilo(String estilo) {
+        this.estilo = estilo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Vehiculo{");
+        sb.append("placa=").append(placa);
+        sb.append(", marca=").append(marca);
+        sb.append(", estilo=").append(estilo);
+        sb.append(", modelo=").append(modelo);
+        sb.append(", capacidadPasajeros=").append(capacidadVehiculo);
+        sb.append('}');
+        return sb.toString();
+    }
+   
 }
