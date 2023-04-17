51eab41bcd01ea69f32e136cc1607be0d8a4ac4e
Gabriel Gutierrez <ggutierrez00078@ufide.ac.cr>
2023-04-16 19:29:10 -0600

    Se agrego el modelo de Usuario y Tiquete

--- src/modelo/Tiquete.java
+++ src/modelo/Tiquete.java
@@ -1,7 +1,24 @@
-
 package modelo;
 
-
 public class Tiquete {
+    
+    private String CantidadPersonas;
+
+    public Tiquete(String CantidadPersonas) {
+        this.CantidadPersonas = CantidadPersonas;
+    }
+
+    public String getCantidadPersonas() {
+        return CantidadPersonas;
+    }
+
+    public void setCantidadPersonas(String CantidadPersonas) {
+        this.CantidadPersonas = CantidadPersonas;
+    }
+
+    @Override
+    public String toString() {
+        return "Tiquete{" + "CantidadPersonas=" + CantidadPersonas + '}';
+    }
     
 }
--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -1,6 +1,44 @@
-
 package modelo;
 
 public class Usuario {
+    private String IdUsuario;
+    private String CodigoUsuario;
+    private String ClaveUsuario;
+
+    public Usuario(String IdUsuario, String CodigoUsuario, String ClaveUsuario) {
+        this.IdUsuario = IdUsuario;
+        this.CodigoUsuario = CodigoUsuario;
+        this.ClaveUsuario = ClaveUsuario;
+    }
+    
+    public String getIdUsuario() {
+        return IdUsuario;
+    }
+
+    public void setIdUsuario(String IdUsuario) {
+        this.IdUsuario = IdUsuario;
+    }
+
+    public String getCodigoUsuario() {
+        return CodigoUsuario;
+    }
+
+    public void setCodigoUsuario(String CodigoUsuario) {
+        this.CodigoUsuario = CodigoUsuario;
+    }
+
+    public String getClaveUsuario() {
+        return ClaveUsuario;
+    }
+
+    public void setClaveUsuario(String ClaveUsuario) {
+        this.ClaveUsuario = ClaveUsuario;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "IdUsuario=" + IdUsuario + ", CodigoUsuario=" + CodigoUsuario + ", ClaveUsuario=" + ClaveUsuario + '}';
+    }
+    
     
 }
