9f85e1f7a161eda1a80d0e70b5304cd7ffe2a198
Andyjrr97 <124108114+Andyjrr97@users.noreply.github.com>
2023-04-17 18:48:19 -0600

    Create ChoferController.java
    se crea el package controller con su clase chofercontroller.

---
+++ src/controller/ChoferController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import interfaces.Crudinterfaces;
+
+public class ChoferController implements Crudinterfaces {
+
+    @Override
+    public void registrar() {
+    }
+
+    @Override
+    public void consultar() {
+    }
+
+    @Override
+    public void editar() {
+    }
+
+    @Override
+    public void eliminar() {
+    }
+
+}
