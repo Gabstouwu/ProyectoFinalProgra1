eec8be6ccf0b3f5469a1fc218e020cc5ad540a65
Andyjrr97 <124108114+Andyjrr97@users.noreply.github.com>
2023-04-17 18:16:20 -0600

    Create Crudinterfaces.java
    se crea el package interface con la clase crudinterface y sus metodos.

---
+++ src/interfaces/Crudinterfaces.java
@@ -0,0 +1,17 @@
+package interfaces;
+
+public interface Crudinterfaces {
+
+    public void registrar();
+
+    public void consultar();
+
+    public void editar();
+
+    public void eliminar();
+    
+    public void validar();
+    
+    public void informe();
+
+}
