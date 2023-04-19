a86cbf170261cb447f18524197ac5dc52634cd65
Andyjrr97 <124108114+Andyjrr97@users.noreply.github.com>
2023-04-18 20:05:51 -0600

    Update VehiculoController.java
    compartir con Gabriel para ver un error

--- src/controller/VehiculoController.java
+++ src/controller/VehiculoController.java
@@ -1,23 +1,57 @@
 package controller;
 
 import interfaces.Crudinterfaces;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import modelo.Vehiculo;
 
 public class VehiculoController implements Crudinterfaces {
 
+    public static ArrayList<Vehiculo> lista_V = new ArrayList<>();
+
     @Override
     public void registrar() {
+        Vehiculo V1 = new Vehiculo("1234", " Hyundai", "slim", "2010", "20");
+        lista_V.add(V1);
+        Vehiculo V2 = new Vehiculo("5678", "Mitsubishi", "small", "2015", "35");
+        lista_V.add(V1);
+        Vehiculo V3 = new Vehiculo("9012", "Volkswagen", "xSmall", "2022", "50");
+        lista_V.add(V1);
+
     }
 
     @Override
     public void consultar() {
+//        String inputplaca;
+//        try {
+//            System.out.println(lista_V.toString());
+//            inputplaca = "1234";
+//            System.out.print(inputplaca);
+//            for (int i = 0; i < lista_V.size(); i++) {
+//                System.out.println(lista_V.get(i).getPlaca());
+//                if (lista_V.get(i)  {
+//                    lista_V == inputplaca
+//                }
+//                
+//                    );
+//                
+//                    break;
+//                }
+//            }
+//        } catch (Exception e) {
+//            JOptionPane.showMessageDialog(null, "Numero de palca incorrecto");
+//        }
+
     }
 
     @Override
     public void editar() {
+         
     }
 
     @Override
     public void eliminar() {
+
     }
-    
+
 }
