53cb41db44bcc4706054ceb60e48db034e735f9a
Gabriel Gutierrez <ggutierrez00078@ufide.ac.cr>
2023-04-18 19:06:49 -0600

    Se creo los controladores de tique, usuario

---
+++ src/controller/TiqueteController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import interfaces.Crudinterfaces;
+
+public class TiqueteController implements Crudinterfaces {
+
+    @Override
+    public void registrar() {
+    }
+
+    @Override
+    public void consultar() {
+    }
+
+    @Override
+    public void editar() {
+    }
+
+    @Override
+    public void eliminar() {
+    }
+    
+}
---
+++ src/controller/UsuarioController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import interfaces.Crudinterfaces;
+
+public class UsuarioController implements Crudinterfaces {
+
+    @Override
+    public void registrar() {
+    }
+
+    @Override
+    public void consultar() {
+    }
+
+    @Override
+    public void editar() {
+    }
+
+    @Override
+    public void eliminar() {
+    }
+    
+}
