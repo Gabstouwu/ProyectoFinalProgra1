8a4d5b36a1a68456a26536eb003b519b2dc1f178
Andyjrr97 <124108114+Andyjrr97@users.noreply.github.com>
2023-04-18 18:03:24 -0600

    Create ViajeController.java
    se crea la clase de ViajeController con sus implementos de la clase crudinterfaces

---
+++ src/controller/ViajeController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import interfaces.Crudinterfaces;
+
+public class ViajeController implements Crudinterfaces  {
+
+    @Override
+    public void registrar() {
+    }
+
+    @Override
+    public void consultar() {
+    }
+
+    @Override
+    public void editar() {
+    }
+
+    @Override
+    public void eliminar() {
+    }
+    
+}
