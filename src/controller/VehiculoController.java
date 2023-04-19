de9d3a91366279df57ee1635e77b0330cf39a7e6
Gabriel Gutierrez <ggutierrez00078@ufide.ac.cr>
2023-04-18 20:44:59 -0600

    Cambios controlador Vehiculos
    Se agrego la parte de consultar vehiculo

--- src/controller/VehiculoController.java
+++ src/controller/VehiculoController.java
@@ -2,56 +2,58 @@ package controller;
 
 import interfaces.Crudinterfaces;
 import java.util.ArrayList;
-import javax.swing.JOptionPane;
 import modelo.Vehiculo;
 
 public class VehiculoController implements Crudinterfaces {
+
+    public Metodos m = new Metodos();
 
     public static ArrayList<Vehiculo> lista_V = new ArrayList<>();
 
     @Override
     public void registrar() {
-        Vehiculo V1 = new Vehiculo("1234", " Hyundai", "slim", "2010", "20");
-        lista_V.add(V1);
-        Vehiculo V2 = new Vehiculo("5678", "Mitsubishi", "small", "2015", "35");
-        lista_V.add(V1);
-        Vehiculo V3 = new Vehiculo("9012", "Volkswagen", "xSmall", "2022", "50");
-        lista_V.add(V1);
-
-    }
-
-    @Override
-    public void consultar() {
-//        String inputplaca;
-//        try {
-//            System.out.println(lista_V.toString());
-//            inputplaca = "1234";
-//            System.out.print(inputplaca);
-//            for (int i = 0; i < lista_V.size(); i++) {
-//                System.out.println(lista_V.get(i).getPlaca());
-//                if (lista_V.get(i)  {
-//                    lista_V == inputplaca
-//                }
-//                
-//                    );
-//                
-//                    break;
-//                }
-//            }
-//        } catch (Exception e) {
-//            JOptionPane.showMessageDialog(null, "Numero de palca incorrecto");
-//        }
 
     }
 
     @Override
     public void editar() {
-         
     }
 
     @Override
     public void eliminar() {
-
     }
 
+    @Override
+    public void consultar() {
+        if (lista_V.isEmpty()) {
+            m.msg("No hay registro de vehiculos");
+        } else {
+
+            String placa = m.getCadena("Ingrese el numero de placa");
+            Vehiculo vehiculo = new Vehiculo();
+            vehiculo.setPlaca("");
+            for (Vehiculo v : lista_V) {
+                if (v.getPlaca().equals(placa)) {
+                    vehiculo = v;
+                    break;
+                }
+            }
+            if (vehiculo.getPlaca().isEmpty()) {
+                m.msg("Vehiculo no existe");
+            } else {
+                m.msg(vehiculo.toString());
+            }
+        }
+    }
+
+    public void CargarDatos() {
+
+        Vehiculo V1 = new Vehiculo("1234", " Hyundai", "slim", "2010", "20");
+        Vehiculo V2 = new Vehiculo("5678", "Mitsubishi", "small", "2015", "35");
+        Vehiculo V3 = new Vehiculo("9012", "Volkswagen", "xSmall", "2022", "50");
+        lista_V.add(V1);
+        lista_V.add(V2);
+        lista_V.add(V3);
+
+    }
 }
