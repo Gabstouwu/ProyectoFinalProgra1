997fd02f1496c272def6f1399868416a57631644
Xhepexz <jjimenez20762@ufide.ac.cr>
2023-04-19 21:22:02 -0600

    Update PersonaView.java
    avance 1 de persona view

--- src/view/PersonaView.java
+++ src/view/PersonaView.java
@@ -1,13 +1,66 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package view;
 
-/**
- *
- * @author castr
- */
+
+import controller.Metodos;
+import controller.PersonaController;
 public class PersonaView {
+
+    public static String id ;
+    public static String numero;
+    public static String correo;
+    public static String nombre;
+    Metodos m = new Metodos();
     
+    public void MenuPersonas() {
+    PersonaController personas = new PersonaController();
+        boolean decision = true;
+        int opcion;
+        String[] opciones = {"Registrar", "Consultar", "Editar", "Elmininar", "Salir"};
+        while (decision == true) {
+            opcion = m.menuBotones("Elija una Opcion", "Elija una opcion:", opciones, "Crear");
+            switch (opcion) {
+                case 0:
+                    personas.registrar();
+                    break;
+                case 1:
+                    consultarPersona();
+                    break;
+                case 2:
+                    editarPersona();
+                    break;
+                case 3:
+                    eliminarPersona();
+                    break;
+                case 4:
+                    decision = false;
+                    break;
+
+            }
+
+        }
+
+    }
+
+    public String crearPersona() {
+
+        id = m.getCadena("Ingrese el Id de la persona: ");
+        nombre = m.getCadena("Ingrese el nombre de la persona: ");
+        numero = m.getCadena("Ingrese el numero de la persona: ");
+        correo = m.getCadena("Ingrese el correo de la persona: ");
+        
+        return id;
+
+    }
+
+    public void consultarPersona() {
+
+    }
+
+    public void editarPersona() {
+
+    }
+
+    public void eliminarPersona() {
+
+    }
 }
