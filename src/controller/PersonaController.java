8c82eb015ee02f4f187775d203809c34bfa08f97
Xhepexz <jjimenez20762@ufide.ac.cr>
2023-04-19 21:20:52 -0600

    Update PersonaController.java
    avance 1 de persona controller

--- src/controller/PersonaController.java
+++ src/controller/PersonaController.java
@@ -1,11 +1,49 @@
 package controller;
 
 import interfaces.Crudinterfaces;
+import java.util.ArrayList;
+import modelo.Persona;
+import view.PersonaView;
 
 public class PersonaController implements Crudinterfaces {
-    
+
+    public static ArrayList<Persona> lista_Persona = new ArrayList();
+    PersonaView persona = new PersonaView();
+    Metodos m = new Metodos();
+    Persona crearPersona = new Persona();
+
     @Override
     public void registrar() {
+        
+        
+        
+
+        try {
+           String id = PersonaView.id ;
+           persona.crearPersona();
+           
+           String nombre = PersonaView.nombre ;
+           String numero =  PersonaView.numero;
+           String correo = PersonaView.correo ;
+            for (int i = 0; i < lista_Persona.size(); i++) {
+                if (lista_Persona.get(i).getIdPersona() != id) {
+                    m.msg(id);
+                    crearPersona.setIdPersona(id);
+                    crearPersona.setNombrePersona(nombre);
+                    crearPersona.setTelefonoPersona(numero);
+                    crearPersona.setCorreo(correo);
+                    lista_Persona.add(crearPersona);
+                    
+                    System.out.println(lista_Persona);
+                    break;
+                    
+                }
+
+            }
+        } catch (Exception e) {
+            m.msg("Id ya existente, ingrese una persona nueva");
+        }
+
     }
 
     @Override
