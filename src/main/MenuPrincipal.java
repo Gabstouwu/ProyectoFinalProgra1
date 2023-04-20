91fd36c5e37bbbc775ff7f3ef8a9306ee19dfb78
Xhepexz <jjimenez20762@ufide.ac.cr>
2023-04-19 21:21:12 -0600

    Update MenuPrincipal.java
    avance 1 de menu principal

--- src/main/MenuPrincipal.java
+++ src/main/MenuPrincipal.java
@@ -1,20 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package main;
-
-/**
- *
- * @author gabri
- */
+import controller.Metodos;
+import view.PersonaView;
+import controller.PersonaController;
 public class MenuPrincipal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Metodos m = new Metodos();
+        PersonaView persona = new PersonaView();
+        PersonaController personaC = new PersonaController();
+        int opcion;
+         do {
+            try {
+               opcion = m.getEntero(
+                        "MENU PRNCIPAL\n"
+                        + "1. Personas\n"
+                        + "2. Registro de personas\n"
+                        + "3. Ventas\n"
+                        + "4. Salir\n"
+                        + "Elija una opción:");
+            } catch (Exception e) {
+                opcion = 0;
+            }
+
+            switch (opcion) {
+                case 1:
+                    persona.MenuPersonas();
+                    //personaC.registrar();
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                  
+                    break;
+
+                case 4:
+                    m.msg("Gracias por usar el sistema, vuelva pronto...");
+                    break;
+                default:
+
+            }
+        } while (opcion != 4);
+    }
     }
     
-}
+
