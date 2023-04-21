4bab2906f0a769406b7b5ca0e89d2399c49fa5c8
Gabriel Gutierrez <ggutierrez00078@ufide.ac.cr>
2023-04-21 12:58:25 -0600

    Se actualiza el usuarioCOntroller
    Porfa comentar las lineas de crud en sus controllers para poder continuar con el proyecto

--- src/controller/UsuarioController.java
+++ src/controller/UsuarioController.java
@@ -1,50 +1,97 @@
 package controller;
 
-import interfaces.Crudinterfaces;
+//import interfaces.Crudinterfaces;
 import java.util.ArrayList;
 import modelo.Usuario;
 
-public class UsuarioController implements Crudinterfaces {
+public class UsuarioController {//implements Crudinterfaces{
 
-    public static ArrayList<Usuario> ListaUsuario = new ArrayList<>(); 
-    
-    
-    
-    @Override
+    public static ArrayList<Usuario> ListaUsuario = new ArrayList<>();
+
+    //@Override
     public void registrar() {
         String NombreUsuario;
         String IdUsuario;
         String ClaveUsuario;
-        
-        NombreUsuario= "Carlos";
-        IdUsuario= "951";
+
+        NombreUsuario = "Carlos";
+        IdUsuario = "951";
         ClaveUsuario = "14789632";
-        
-        
-        Usuario NevoUsuario = new Usuario(NombreUsuario, IdUsuario, ClaveUsuario);
-        
-        ListaUsuario.add(NevoUsuario);
-        
-        
+
+        Usuario NuevoUsuario = new Usuario(NombreUsuario, IdUsuario, ClaveUsuario);
+
+        ListaUsuario.add(NuevoUsuario);
+
     }
 
-    @Override
+    //@Override
+
+    //Estoy usando el método equals() para comparar la cadena IdUsuario con la cadena inputParaBuscar
     public void consultar() {
+        String inputParaBuscar = "951";
+        try {
+            for (int i = 0; i < ListaUsuario.size(); i++) {
+                if (ListaUsuario.get(i).getIdUsuario().equals(inputParaBuscar)) {
+                    System.out.println(ListaUsuario.get(i));
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("NO se encuentra el id");
+        }
     }
 
-    @Override
+   // @Override
     public void editar() {
+
+        String inputParaBuscar = "951";
+
+        int indexId = -1;
+
+        if (indexId != -1) {
+            String CodigoNuevo;
+            String ClaveNueva;
+            CodigoNuevo = "Mario";
+            ClaveNueva = "951";
+
+            for (int i = 0; i < ListaUsuario.size(); i++) {
+                if (ListaUsuario.get(i).getIdUsuario().equals(inputParaBuscar)) {
+
+                    ListaUsuario.get(indexId).setCodigoUsuario(CodigoNuevo);
+                    ListaUsuario.get(indexId).setClaveUsuario(ClaveNueva);
+                    break;
+                }
+            }
+
+            System.out.println("Modificación realizada con exito.");
+        } else {
+            System.out.println("ID no existente");
+        }
+
     }
 
-    @Override
-    public void eliminar() {
+    //@Override
+    public boolean eliminar(String idEliminar) {
+        
+        idEliminar = "963";
+
+        for (int i = 0; i < ListaUsuario.size(); i++) {
+
+            if (ListaUsuario.get(i).getIdUsuario().equals(idEliminar)) {
+
+                ListaUsuario.remove(i);
+                System.out.println("Modificación realizada con exito.");
+                return true;
+            }
+        }
+        return false;
     }
 
     public void CargarDatos() {
 
-        Usuario usuario1 = new Usuario ("123","Juan","963");
-        Usuario usuario2 = new Usuario("456","Maria","852");
-        Usuario usuario3 = new Usuario("789","Luis","741");;
+        Usuario usuario1 = new Usuario("123", "Juan", "963");
+        Usuario usuario2 = new Usuario("456", "Maria", "852");
+        Usuario usuario3 = new Usuario("789", "Luis", "741");;
         ListaUsuario.add(usuario1);
         ListaUsuario.add(usuario2);
         ListaUsuario.add(usuario3);
---
+++ src/view/UsuarioView.java
@@ -0,0 +1,34 @@
+package view;
+
+import controller.UsuarioController;
+import controller.Metodos;
+
+//public class;
+////        public void MenuUsuario() {
+////    UsuarioController usuario = new UsuarioController();
+////        boolean decision = true;
+////        int opcion;
+////        String[] opciones = {"Registrar", "Consultar", "Editar", "Elmininar", "Salir"};
+////        while (decision == true) {
+////           // opcion = m.menuBotones("Elija una Opcion", "Elija una opcion:", opciones, "Crear");
+////            switch (opcion) {
+////                case 0:
+////                    break;
+////                case 1:
+////                    break;
+////                case 2:
+////                    
+////                    break;
+////                case 3:
+////                    
+////                    break;
+////                case 4:
+////                    decision = false;
+////                    break;
+////
+////            }
+////
+////        }
+////
+////    }
+////}
