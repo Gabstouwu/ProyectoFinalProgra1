e84ba61f76fc768c04a41c74898dec6c576267a8
Andyjrr97 <124108114+Andyjrr97@users.noreply.github.com>
2023-04-22 12:53:22 -0600

    Update ViajeController.java
    se crea calse viajecontroller con sus variables de registrar, consultar,editar y eliminar

--- src/controller/ViajeController.java
+++ src/controller/ViajeController.java
@@ -1,23 +1,51 @@
 package controller;
 
-import interfaces.Crudinterfaces;
+import java.util.ArrayList;
+import modelo.Viaje;
+import view.ViajeView;
+import controller.Metodos;
 
-public class ViajeController implements Crudinterfaces  {
-
-    @Override
+public class ViajeController {
+    public static ArrayList<Viaje> lista_Viaje = new ArrayList();
+    
+    Metodos m = new Metodos();
+    Viaje crearViaje = new Viaje();
+    
     public void registrar() {
+        
     }
-
-    @Override
+   
     public void consultar() {
+        
     }
 
-    @Override
     public void editar() {
+        
     }
 
-    @Override
     public void eliminar() {
+        
     }
     
+    
+     public boolean existe(String idViaje) {
+        boolean valor = false;
+        for (Viaje vehiculo : lista_Viaje) {
+            if (vehiculo.getIdViaje().equals(idViaje)) {
+                valor = true;
+            }
+        }
+
+        return valor;
+    }
+        
+        public int poscision(String idViaje){
+    int pos=0;
+    for (Viaje viaje : lista_Viaje) {
+            if (viaje.getIdViaje().equals(idViaje)) {
+                pos = viaje.getIdViaje().indexOf(idViaje);
+            }
+        }
+    return pos;
+    }
 }
