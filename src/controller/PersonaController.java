2e7531589dc122347da1a7f44f4c7be2f1caf84c
Xhepexz <jjimenez20762@ufide.ac.cr>
2023-04-21 21:38:06 -0600

    Update PersonaController.java
    metodo consultar y eliminar hechos y funcionales

--- src/controller/PersonaController.java
+++ src/controller/PersonaController.java
@@ -49,31 +49,46 @@ public class PersonaController {
     }
 
     public void consultar() {
-        PersonaView persona = new PersonaView();
-        String id = persona.id;
+
         if (lista_Persona.isEmpty()) {
             m.msg("Aun no hay informacion en la lista");
         } else {
-            try {
-
-                if (existe(id)) {
-                    for (int i = 0; i< lista_Persona.size(); i++){
-                    m.msg(lista_Persona.get(i).toString());
-                    break;
+            PersonaView persona = new PersonaView();
+            persona.consultarPersona();
+            String id = persona.id;
+            if (existe(id)) {
+                for (int i = 0; i < lista_Persona.size(); i++) {
+                    if (lista_Persona.get(i).getIdPersona().equals(id)) {
+                        m.msg(lista_Persona.get(i).toString());
+                        break;
                     }
-                    
-
                 }
-            } catch (Exception e) {
-                m.msg("El número de chasis ingresado es incorrecto.");
+
+            } else {
+                m.msg("Persona no existe");
             }
         }
     }
 
     public void editar() {
+        
     }
 
     public void eliminar() {
+         if (lista_Persona.isEmpty()) {
+            m.msg("Aun no hay informacion en la lista");
+        } else {
+            PersonaView persona = new PersonaView();
+            persona.eliminarPersona();
+            String id = persona.id;
+            if (poscision(id) < lista_Persona.size()) {
+               
+                    lista_Persona.remove(poscision(id));
+                    m.msg("Persona Eliminada"+ lista_Persona.toString());
+            }
+            }
+            
+        
     }
 
     public boolean existe(String id) {
@@ -85,5 +100,15 @@ public class PersonaController {
         }
 
         return valor;
+    }
+    
+    public int poscision(String id){
+    int pos=0;
+    for (Persona personas : lista_Persona) {
+            if (personas.getIdPersona().equals(id)) {
+                pos = personas.getIdPersona().indexOf(id);
+            }
+        }
+    return pos;
     }
 }
